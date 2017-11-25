5e94fd27af262c63d6665892324f5ddd634edfe0
hsiungwilliam <hsiung.william.ib@gmail.com>
2017-11-24 22:36:44 -0500

    Changed Contest views

--- modules/contests/client/controllers/contests.client.controller.js
+++ modules/contests/client/controllers/contests.client.controller.js
@@ -6,19 +6,49 @@
     .module('contests')
     .controller('ContestsController', ContestsController);
 
-  ContestsController.$inject = ['$scope', '$state', '$window', 'Authentication', 'contestResolve'];
+  ContestsController.$inject = ['$scope', '$state', '$window', 'Authentication', 'contestResolve', '$timeout', 'ContestsService'];
 
-  function ContestsController ($scope, $state, $window, Authentication, contest) {
+  function ContestsController ($scope, $state, $window, Authentication, contest, $timeout, ContestsService) {
     var vm = this;
 
     vm.authentication = Authentication;
     vm.contest = contest;
-    // vm.submission = submission;
+    // vm.contest = contest;
     vm.error = null;
     vm.form = {};
     vm.remove = remove;
     vm.save = save;
-    console.log("contestid" + contest._id);
+    //console.log("contestid" + contest._id);
+    vm.contests = ContestsService.query();
+
+    //To upload file
+    $scope.photoChanged2 = function (files) {
+      if (files.length > 0 && files[0].name.match(/\.(png|jpg|jpeg|pdf|gif)$/)) {
+        $scope.uploading = true;
+        var file = files[0];
+        var fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+        fileReader.onload = function (e) {
+          $timeout(function () {
+            // Render art.
+            $scope.thumbnail = {};
+            $scope.thumbnail = e.target.result;
+            //console.log("thumbail is" + $scope.art);
+            var day = new Date();
+            var d = day.getDay();
+            var h = day.getHours();
+            //vm.contest.thumbnail = 'modules/contests/client/img/' + d + '_' + h + '_' + files[0].name;
+            vm.contest.thumbnail = $scope.thumbnail;
+            //console.log(JSON.stringify(vm.contest, null, 4));
+            $scope.uploading = false;
+            $scope.message = false;
+          });
+        };
+      } else {
+        $scope.thumbnail = {};
+        $scope.message = false;
+      }
+    };
 
     // Remove existing Contest
     function remove() {
